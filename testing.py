8d45e91af2757be970e3152262544cb02d0b64ce
Dom <32805457+dsds268@users.noreply.github.com>
2024-08-05 10:24:09 +1000

    inital

---
+++ testing.py
@@ -0,0 +1,17 @@
+
+print('Enter Input 1: ')
+A = int(input())
+
+print('Enter Input 2: ')
+B = int(input())
+
+stat_1 = (A + B)
+stat_2 = stat_1 * B
+stat_3 = stat_2 - 5
+
+valid_opr = ['+', '-', '*']
+
+ 
+
+print(C)
+
